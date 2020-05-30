c326f303f3639acf97428158a2aab650811cb29a
Johnny Stene <jhonnystene@gmail.com>
2020-05-29 19:53:13 -0600

    OOF

--- index.php
+++ index.php
@@ -2,10 +2,31 @@
 <html>
 	<head>
 		<title>PizzaPolice (Alpha)</title>
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-		<?php 
-			print("Test!");
-		?>
+		<div class="header">
+			<h1>PizzaPolice Alpha</h1>
+		</div>
+		<div class="content">
+			<h2>COVID-19 Statistics Worldwide</h2>
+			<p id="worldwideNewCases"></p><br>
+			<p id="worldwideTotalCases"></p><br>
+			<p id="worldwideNewDeaths"></p><br>
+			<p id="worldwideTotalDeaths"></p><br>
+			<h2>COVID-19 Statistics Here</h2>
+			<p id="hereNewCases"></p><br>
+			<p id="hereTotalCases"></p><br>
+			<p id="hereNewDeaths"></p><br>
+			<p id="hereTotalDeaths"></p><br>
+		</div>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script>
+			$.get("https://ipinfo.io/json", function(data) {
+				console.log(data);
+				alert(data);
+			});
+		</script>
 	</body>
 </html>

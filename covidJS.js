ecf8e9ef06be095a54903a08520fa08e1db77d4d
Johnny Stene <jhonnystene@gmail.com>
2020-05-30 09:11:30 -0600

    Trying something

--- covidJS.js
+++ covidJS.js
@@ -8,7 +8,29 @@ var countryCode = 0;
 $.get("https://ipinfo.io/json", function(data) {
 	countryCode = data["country"];
 	document.getElementById("statsHere").innerHTML = "COVID-19 Statistics in " + getCountryName(countryCode);
-	getStats(countryCode);
+	// Get COVID stats
+	$.get("https://api.covid19api.com/summary", function(data) {
+		var worldwideNew = data["Global"]["NewConfirmed"];
+		var worldwideTotal = data["Global"]["TotalConfirmed"];
+		var worldwideNewDeaths = data["Global"]["NewDeaths"];
+		var worldwideTotalDeaths = data["Global"]["TotalDeaths"];
+		document.getElementById("worldwideNewCases").innerHTML = worldwideNew + " new cases today";
+		document.getElementById("worldwideTotalCases").innerHTML = worldwideTotal + " total cases";
+		document.getElementById("worldwideNewDeaths").innerHTML = worldwideNewDeaths + " new deaths today";
+		document.getElementById("worldwideTotalDeaths").innerHTML = worldwideTotalDeaths + " total deaths";
+		data["Countries"].forEach(function(item, index) {
+			if(item["CountryCode"] == countryCode) {
+						var hereNew = item["NewConfirmed"];
+						var hereTotal = item["TotalConfirmed"];
+						var hereNewDeaths = item["NewDeaths"];
+						var hereTotalDeaths = item["TotalDeaths"];
+						document.getElementById("hereNewCases").innerHTML = hereNew + " new cases today";
+						document.getElementById("hereTotalCases").innerHTML = hereTotal + " total cases";
+						document.getElementById("hereNewDeaths").innerHTML = hereNewDeaths + " new deaths today";
+						document.getElementById("hereTotalDeaths").innerHTML = hereTotalDeaths + " total deaths";
+				}
+		});
+	});
 });
 
 function getStats(countryCode) {
@@ -33,19 +55,6 @@ function tryStatsUpdate() {
 	if(countryCode == 0) setTimeout(tryStatsUpdate, 1000);
 	getStats(countryCode);
 }
-
-// Get COVID stats
-$.get("https://api.covid19api.com/summary", function(data) {
-	var worldwideNew = data["Global"]["NewConfirmed"];
-	var worldwideTotal = data["Global"]["TotalConfirmed"];
-	var worldwideNewDeaths = data["Global"]["NewDeaths"];
-	var worldwideTotalDeaths = data["Global"]["TotalDeaths"];
-	document.getElementById("worldwideNewCases").innerHTML = worldwideNew + " new cases today";
-	document.getElementById("worldwideTotalCases").innerHTML = worldwideTotal + " total cases";
-	document.getElementById("worldwideNewDeaths").innerHTML = worldwideNewDeaths + " new deaths today";
-	document.getElementById("worldwideTotalDeaths").innerHTML = worldwideTotalDeaths + " total deaths";
-	setTimeout(tryStatsUpdate, 1000);
-});
 
 // Get news
 $.get("https://covid19-us-api.herokuapp.com/news", function(data) {
